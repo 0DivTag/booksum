177d4b0fec435b6b3f200c39a6409cffc8e03222
Tim Richter <Tim.Chris.Richter@gmail.com>
2019-04-10 06:19:06 +0200

    Added 3 Components: Header, Generator, Footer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Header from "./container/Header/Header";
+import Generator from "./container/Generator/Generator";
+import Footer from "./container/Footer/Footer";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header />
+        <Generator />
+        <Footer />
       </div>
     );
   }
